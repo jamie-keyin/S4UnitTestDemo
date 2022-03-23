5c9b434f74575236df87c856dc100c6a28324fa5
jamie.cornick <jamie.cornick@keyin.com>
2022-03-23 10:22:08 -0230

    Added query methods using join tables (entities).

--- initial/src/main/java/com/example/restservice/domain/StudentRepository.java
+++ initial/src/main/java/com/example/restservice/domain/StudentRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "student", path = "student")
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByLastName(@Param("last_name")String lastName);
+    List<Student> findByCourses_Name(@Param("course_name")String courseName);
+    List<Student> findByPrimaryAddress_CityAndPrimaryAddress_PostalCode(@Param("city")String city, @Param("postal_code")String postalCode);
 }
